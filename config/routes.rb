e665caecfa407090b043360b996ff256d7991238
Andrea Olarte <andy_fn_14@hotmail.com>
2018-11-14 11:12:53 -0600

    Add Frida and Mentor views
    Add the devise views for the Frida and Mentor users and adding the
    needed configurations in config/routes.rb along with the root page
    (index).

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :mentors
-  devise_for :fridas
+  get 'welcome/index'
+  devise_for :mentors, path: 'mentors', controllers: { sessions: 'mentors/sessions' }
+  devise_for :fridas, path: 'fridas', controllers: { sessions: 'fridas/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'welcome#index'
 end
