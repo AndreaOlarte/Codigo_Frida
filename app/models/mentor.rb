a80580c64b932eb251fab75e937ed265bb0459f0
Andrea Olarte <andy_fn_14@hotmail.com>
2018-11-20 23:00:40 -0600

    Add Frida and Mentor model associations
    The association between those Models and the other ones were added.
    
    Fridas belongs to a team, while a Mentor has many teams and also has
    many feedback.

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -3,4 +3,7 @@ class Mentor < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :teams
+  has_many :feedbacks
 end
