9b0ecffae967054242ff2a1873e107062e41de9b
Andrea Olarte <andy_fn_14@hotmail.com>
2018-11-20 22:59:05 -0600

    Add extra params to Mentor model
    It is important to indicate the devise controllers there are more
    params for the Mentor model to have so it allows to enter them with the
    permit method.
    
    Also, fields for the input of those attributes were added for the sign up
    views.

--- app/controllers/mentors/registrations_controller.rb
+++ app/controllers/mentors/registrations_controller.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Mentors::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :authenticate_user!
+  
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -41,19 +43,19 @@ class Mentors::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :birthdate, :company, :phone, :avatar, :expertise])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :last_name, :company, :phone, :avatar])
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    # teams_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
